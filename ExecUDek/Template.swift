c8437153038eebac2776804ae10987587ccf0135
ganleyth <tganley3@gmail.com>
2017-07-25 16:08:57 -0600

    Add Card model class and a placeholder for the Template enum

---
+++ ExecUDek/Template.swift
@@ -0,0 +1,13 @@
+//
+//  Template.swift
+//  ExecUDek
+//
+//  Created by Thomas Ganley on 7/25/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import Foundation
+
+enum Template: String {
+    case one
+}
