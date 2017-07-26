993df136befe06d7d69efb232279fd0f5710be63
Arnold <arnoldplas@gmail.com>
2017-07-26 10:21:16 -0600

    Card template
    Card template

---
+++ ExecUDek/CardTemplateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardTemplateViewController.swift
+//  ExecUDek
+//
+//  Created by Arnold Mukasa on 7/26/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class CardTemplateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
