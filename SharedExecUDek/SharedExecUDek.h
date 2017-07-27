c1179bc7bf90e0ad2e677f30e148b0f9dc7fa567
ganleyth <tganley3@gmail.com>
2017-07-27 09:37:51 -0600

    Add shared framework

---
+++ SharedExecUDek/SharedExecUDek.h
@@ -0,0 +1,19 @@
+//
+//  SharedExecUDek.h
+//  SharedExecUDek
+//
+//  Created by Thomas Ganley on 7/26/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedExecUDek.
+FOUNDATION_EXPORT double SharedExecUDekVersionNumber;
+
+//! Project version string for SharedExecUDek.
+FOUNDATION_EXPORT const unsigned char SharedExecUDekVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedExecUDek/PublicHeader.h>
+
+
