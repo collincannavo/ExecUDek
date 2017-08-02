fc09aa1e3bfa9d5cc16581c1ca0b88e613dd3142
Bop <collin.cannavo@gmail.com>
2017-08-02 11:13:55 -0600

    Added Alert for email fail

--- ExecUDek/EmailViewController.swift
+++ ExecUDek/EmailViewController.swift
@@ -28,34 +28,39 @@ extension UserProfileTableViewController {
         } else {
             NSLog("This device cannot send email")
             print("This device cannot send email")
+            let alertController = UIAlertController(title: "Is your Mail App on?", message: "Please ensure your Apple Mail app is active in your Settings", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
         }
     }
 
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         
         switch result {
-        case MFMailComposeResult.cancelled:
+        case .cancelled:
             NSLog("%@", "Email was cancelled")
             let alert = UIAlertController(title: "Your email has been saved", message: "", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
             alert.addAction(okAction)
             present(alert, animated: true, completion: nil)
             self.dismiss(animated: true, completion: nil)
-        case MFMailComposeResult.saved:
+        case .saved:
             NSLog("Email has been saved")
             let alert = UIAlertController(title: "Your email has been saved", message: "", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
             alert.addAction(okAction)
             present(alert, animated: true, completion: nil)
             self.dismiss(animated: true, completion: nil)
-        case MFMailComposeResult.failed:
+        case .failed:
             NSLog("Your email failed to send")
             let alert = UIAlertController(title: "Your email failed to send!", message: "", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
             alert.addAction(okAction)
             present(alert, animated: true, completion: nil)
             self.dismiss(animated: true, completion: nil)
-        case MFMailComposeResult.sent:
+        case .sent:
             NSLog("Your email has been sent!")
             let alert = UIAlertController(title: "Your email was sent successfully!", message: "", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
