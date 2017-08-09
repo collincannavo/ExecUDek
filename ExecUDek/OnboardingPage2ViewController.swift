c46a15d7010c058bdfa01ffa54c01df80a7b7e7d
Bop <collin.cannavo@gmail.com>
2017-08-09 11:11:33 -0600

    Added buttons and constraints

---
+++ ExecUDek/OnboardingPage2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingPage2ViewController.swift
+//  ExecUDek
+//
+//  Created by Collin Cannavo on 8/9/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingPage2ViewController: UIViewController {
+
+    @IBOutlet weak var arrow: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
---
+++ ExecUDek/OnboardingPage3ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  OnboardingPage3ViewController.swift
+//  ExecUDek
+//
+//  Created by Collin Cannavo on 8/9/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingPage3ViewController: UIViewController {
+
+    @IBOutlet weak var arrow: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
