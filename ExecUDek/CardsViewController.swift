dbd01b95bbe3de663339972d6b982b37940e3a21
Arnold <arnoldplas@gmail.com>
2017-08-13 16:31:57 -0600

    Code Refactor
    Code Refactor

--- ExecUDek/CardsViewController.swift
+++ ExecUDek/CardsViewController.swift
@@ -13,14 +13,20 @@ import ContactsUI
 
 class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, ActionSheetDelegate {
     
+    // MARK: Properties
     var filteredCardsArray: [Card] = []
     var newContact: CNMutableContact?
     var isRefreshing = false
     let overlap: CGFloat = -120.0
+    var refreshControl = UIRefreshControl()
     
-    // MARK: - Outlets
+    // MARK: Outlets
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBAction func multipeerButtonTapped(_ sender: UIBarButtonItem) {
+        customNavigationController.confirmChangeOfMultipeer()
+    }
     
+    // MARK: View lifecycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if (PersonController.shared.currentPerson?.initialCardsFetchComplete ?? false) {
@@ -29,47 +35,21 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         collectionView.reloadData()
     }
     
-    @IBAction func multipeerButtonTapped(_ sender: UIBarButtonItem) {
-        customNavigationController.confirmChangeOfMultipeer()
-    }
-    
-    var refreshControl = UIRefreshControl()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
-        
-        
         NotificationCenter.default.addObserver(self, selector: #selector(refresh), name: Constants.cardsFetchedNotification, object: nil)
-        
-        
         let bundle = Bundle(identifier: "com.ganleyApps.SharedExecUDek")
         let cardXIB = UINib(nibName: "CardCollectionViewCell", bundle: bundle)
-        
         collectionView.register(cardXIB, forCellWithReuseIdentifier: "collectionCardCell")
         collectionView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(fetchCards), for: .valueChanged)
-        
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController?.navigationBar.shadowImage = UIImage()
-        navigationController?.navigationBar.isTranslucent = true
-        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
-        UIApplication.shared.statusBarStyle = .lightContent
-        
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
-        swipeLeft.direction = .left
-        self.view.addGestureRecognizer(swipeLeft)
-        
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
-        swipeRight.direction = .right
-        self.view.addGestureRecognizer(swipeRight)
-        
+        setUpNavBarNSwipeGesture()
         checkForInitialLoad()
     }
     
     // MARK: - Collection view data source
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return PersonController.shared.currentPerson?.sortedCards.count ?? 0
     }
@@ -107,7 +87,6 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         setupCardTableViewCellBorderColor(cell)
         collectionView.bringSubview(toFront: cell)
         cell.actionSheetDelegate = self
-        //placeCardInOrder(forIndex: indexPath.row)
         cell.isHidden = false
         return cell
     }
@@ -137,7 +116,21 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         }
     }
     
-    // Card cell action sheet delegate
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        guard let customCell = cell as? CardCollectionViewCell else { return }
+        
+        if indexPath.row % 3 == 0 {
+            customCell.changeBackgroundToBlue()
+        } else if indexPath.row % 3 == 1 {
+            customCell.changeBackgroundToRed()
+        } else if indexPath.row % 3 == 2 {
+            customCell.changeBackgroundToOrange()
+        }
+        placeCardInOrder(forIndex: indexPath.row)
+    }
+    
+    // Cardcell ActionSheet Delegate
     func actionSheetSelected(cellButtonTapped: UIButton, cell: CardCollectionViewCell) {
         let alertController = UIAlertController(title: "Share Business Card", message: "", preferredStyle: .actionSheet)
         let contactButton = UIAlertAction(title: "Add to contacts", style: .default) { (_) in
@@ -180,7 +173,21 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         }
     }
     
-    // MARK: - Helper methods
+    // MARK: Methods
+    fileprivate func setUpNavBarNSwipeGesture() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        UIApplication.shared.statusBarStyle = .lightContent
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+    }
+    
     func refresh() {
         activityIndicator.stopAnimating()
         ActivityIndicator.animateAndRemoveIndicator(indicatorView, from: self.view)
@@ -189,17 +196,16 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         collectionView.reloadData()
     }
     
-    func setupCardTableViewCellShadow(_ cell: CardCollectionViewCell) {
+    fileprivate func setupCardTableViewCellShadow(_ cell: CardCollectionViewCell) {
         cell.layer.shadowOpacity = 1.0
         cell.layer.shadowRadius = 4
         cell.layer.shadowOffset = CGSize(width: 0, height: 4)
         cell.layer.shadowColor = UIColor.black.cgColor
     }
     
-    func setupCardTableViewCellBorderColor(_ cell: CardCollectionViewCell) {
+    fileprivate func setupCardTableViewCellBorderColor(_ cell: CardCollectionViewCell) {
         cell.layer.borderWidth = 10
         cell.layer.borderColor = UIColor.clear.cgColor
-        
     }
     
     func fetchCards() {
@@ -207,6 +213,10 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         guard refreshControl.isRefreshing else { return }
         activityIndicator.startAnimating()
         ActivityIndicator.addAndAnimateIndicator(indicatorView, to: view)
+        clouldKitFetch()
+    }
+    
+    func clouldKitFetch(){
         CloudKitContoller.shared.fetchCurrentUser { (success, currentPerson) in
             if success && (currentPerson != nil) {
                 CardController.shared.fetchReceivedCards { (success) in
@@ -216,7 +226,7 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
                             self.refresh()
                         }
                     }
-                    DispatchQueue.main.async {                        
+                    DispatchQueue.main.async {
                         self.activityIndicator.stopAnimating()
                         ActivityIndicator.animateAndRemoveIndicator(self.indicatorView, from: self.view)
                         if self.refreshControl.isRefreshing { self.refreshControl.endRefreshing() }
@@ -232,23 +242,7 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-        guard let customCell = cell as? CardCollectionViewCell else { return }
-        
-        if indexPath.row % 3 == 0 {
-            customCell.changeBackgroundToBlue()
-        } else if indexPath.row % 3 == 1 {
-            customCell.changeBackgroundToRed()
-        } else if indexPath.row % 3 == 2 {
-            customCell.changeBackgroundToOrange()
-        }
-        
-        placeCardInOrder(forIndex: indexPath.row)
-    }
-    
-    func checkForInitialLoad() {
-        
+    fileprivate func checkForInitialLoad() {
         if let person = PersonController.shared.currentPerson {
             if !person.initialCardsFetchComplete {
                 activityIndicator.startAnimating()
@@ -260,11 +254,19 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         }
     }
     
-    func addContact(for card: Card) {
-        
+    fileprivate func addContact(for card: Card) {
+        contactSetUp(card: card)
+        guard let newContactUnwrapped = newContact else { return }
+        let contactView = CNContactViewController(for: newContactUnwrapped)
+        let navigationView = UINavigationController(rootViewController: contactView)
+        let dismissButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(dismissView))
+        contactView.navigationItem.leftBarButtonItem = dismissButton
+        present(navigationView,animated: true, completion: nil)
+    }
+    
+    fileprivate func contactSetUp(card: Card) {
         newContact = CNMutableContact()
         newContact?.note = "ExecUDek App Business Card"
-        
         newContact?.givenName = card.name
         if let cell = card.cell {
             let phone = CNLabeledValue(label: CNLabelWork, value: CNPhoneNumber(stringValue:cell))
@@ -284,14 +286,6 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
             let workEmail = CNLabeledValue(label:CNLabelWork, value: NSString(string: email))
             newContact?.emailAddresses = [workEmail]
         }
-        
-        guard let newContactUnwrapped = newContact else { return }
-        
-        let contactView = CNContactViewController(for: newContactUnwrapped)
-        let navigationView = UINavigationController(rootViewController: contactView)
-        let dismissButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(dismissView))
-        contactView.navigationItem.leftBarButtonItem = dismissButton
-        present(navigationView,animated: true, completion: nil)
     }
     
     func dismissView() {
@@ -310,7 +304,7 @@ class CardsViewController: MultipeerEnabledViewController, UICollectionViewDataS
         }
     }
     
-    func placeCardInOrder(forIndex index: Int) {
+    fileprivate func placeCardInOrder(forIndex index: Int) {
         for i in index...(collectionView.numberOfItems(inSection: 0) - 1) {
             let indexPath = IndexPath(row: i, section: 0)
             guard let newTopCell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell else { continue }
